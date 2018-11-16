f4bb589c326ae6ca6267c0cd0de422025d2b8df4
Electroolysis <aghhaghhhh@gmail.com>
2018-11-16 11:27:59 -0500

    not breathe

---
+++ src/OratioDEQueue.java
@@ -0,0 +1,7 @@
+public class OratioDEQueue<E> {
+
+    public OratioDEQueue() {
+
+    }
+
+}
---
+++ src/OratioTree.java
@@ -0,0 +1,19 @@
+
+
+/**
+ * OratioTree.java
+ * Modular tree data structure for the Oratio program
+ * @author Eric Ke
+ * Last Updated: 2018/11/16
+ *
+ */
+public class OratioTree<T> {
+
+
+    public OratioTree() {
+
+
+
+    }
+
+}
