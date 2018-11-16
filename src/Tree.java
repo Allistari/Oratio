199819da2d312e5dfaa7f46c20e10d36d2a5d851
joeychik <joey.chik0103@gmail.com>
2018-11-16 00:27:15 -0500

    added several new class files

---
+++ src/Tree.java
@@ -0,0 +1,16 @@
+public class Tree<T> {
+    private Node<T> root;
+    private int preset;
+
+    public Node get() {
+        return;
+    }
+
+    public int getPreset() {
+        return preset;
+    }
+
+    public void setPreset(int preset) {
+        this.preset = preset;
+    }
+}
