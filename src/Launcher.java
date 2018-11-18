bace3542e7ee81764f4db149479aedc5002a5a93
joeychik <joey.chik0103@gmail.com>
2018-11-18 16:41:17 -0500

    idk what im doing help

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,3 +1,6 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Launcher.java
  * main class which ties everything together
@@ -6,17 +9,40 @@
  * @author Angelina Zhang
  * @author Eric Ke
  * created 2018-11-15
- * last modified 2018-11-15
+ * last modified 2018-11-18
  */
 
 public class Launcher {
-    // put variables n stuff here
+    private String phoneticSpelling;
+    private int preset;
 
     /**
      * main method
      * @param args
      */
     public static void main(String[] args) {
-        // put stuff here???
+        new Launcher();
+    }
+
+    Launcher() {
+        //new Display();
+
+        //new OratioTree;
+
+    }
+
+    public void generatePhoneticSpelling(String word) {
+
+    }
+
+    public String getPhoneticSpelling() {
+        return phoneticSpelling;
+    }
+
+    private class InputPanelListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
     }
 }
