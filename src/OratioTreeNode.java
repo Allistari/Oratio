caf7698c2c53fc02d5804cf0f596615feb374e1c
Electroolysis <aghhaghhhh@gmail.com>
2018-11-19 10:42:03 -0500

    treeeee node

---
+++ src/OratioTreeNode.java
@@ -0,0 +1,14 @@
+public class OratioTreeNode<E> {
+
+    private E item;
+    private OratioTreeNode<E>[] children;
+
+    public OratioTreeNode(E item) {
+        this.item = item;
+
+    }
+
+    public void addLeftChild(E item) {
+
+    }
+}
