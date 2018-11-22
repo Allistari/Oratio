010c2e75066ab4813d8d5f29986bbacbc8f162e4
joeychik <joey.chik0103@gmail.com>
2018-11-22 11:32:05 -0500

    updated jsongenerator

--- src/JSONGenerator.java
+++ src/JSONGenerator.java
@@ -8,23 +8,54 @@ import java.io.Writer;
 
 public class JSONGenerator {
     private Gson gson;
-
+    
     JSONGenerator() {
         this.gson = new Gson();
     }
-
+    
     /**
      *
      * @param m
      * @param file
      * @return
      */
-    public boolean generateJSON(MouthShape m, File file) {
+    public boolean generateJSON(MouthShape[] mArray, File file) {
         try {
             JsonWriter writer = new JsonWriter(new FileWriter(file));
         } catch(IOException e) {
             return false;
         }
+        
+        writer.beginArray();
+        
+        for (MouthShape m : mArray) {
+            writeMouthShape(writer, m);
+        }
+        
+        writer.endArray();
+        
+        writer.close();
+        
         return true;
+    }
+    
+    private void writeMouthShape(JsonWriter writer, MouthShape m) {
+        writer.beginObject();
+        
+        writer.name("fileName").value(m.getFileName());
+        writer.name("spelling");
+        writeArray(writer, m.getSpelling());
+        
+        writer.endObject();
+    }
+    
+    private void writeArray(JsonWriter writer, String[] strings) {
+        writer.beginArray();
+        
+        for (String string : strings) {
+            writer.value(string);
+        }
+        
+        writer.endArray();
     }
 }
